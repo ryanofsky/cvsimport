83cb81449eb9fe92f91fd01ba5f7825b8dde8843
Russell Yanofsky <russ@yanofsky.org>
2003-06-15 22:13:47 +0000

    - added testing command line option

--- cvsimport.php
+++ cvsimport.php
@@ -2,13 +2,20 @@
 
 require_once('cvsimport.inc');
 
-if ($_SERVER['argc'] != 2)
+$options = array(array('test', 't', 1, false));
+$args = strip_options($_SERVER['argv'], $options, $opt_values);
+
+if (count($args) != 2)
 {
-  print("Usage: php cvsimport.php config.inc\n");
+  print("Usage: php cvsimport.php [OPTION] config.inc\n\n"
+      . "  -t, --test=DIRECTORY   after creating repository, attempt to\n"
+      . "                         check out different versions of code to\n" 
+      . "                         the specified directory and see if it\n"
+      . "                         matches the imported code.\n");
   exit();
 }
 
-$file = $_SERVER['argv'][1];
+$file = $args[1];
 
 if (!file_exists($file))
 {
@@ -32,6 +39,10 @@ $nl->save();
 unlink($BLANKFILE);
 if (function_exists('CleanUp')) CleanUp();
 
-$nl->checkout();
+if (($test = $opt_values['test']) !== false)
+{
+  if (!is_dir($test)) die("Testing directory '$test' does not exist.");
+  $nl->checkout($test);
+}
 
 ?>
